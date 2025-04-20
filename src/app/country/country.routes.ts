c7f1396ac68ea85a469668f5a1a6bb6b06dd355a
soycarlosherrera <carlos4505@gmail.com>
2025-04-19 22:31:05 -0500

    Paths and folder structure

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+
+
+export const countryRoutes: Routes = [
+  {
+    path: '',
+    component: ByCapitalPageComponent,
+  },
+];
+
+export default countryRoutes;
