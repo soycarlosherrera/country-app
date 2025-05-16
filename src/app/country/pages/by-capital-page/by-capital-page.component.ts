abaeb6f7520d430aa5f5ecb3762972df9d91af1d
soycarlosherrera <carlos4505@gmail.com>
2025-05-15 19:58:04 -0500

    add country service

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -9,6 +10,14 @@ import { CountryListComponent } from "../../components/country-list/country-list
 })
 export class ByCapitalPageComponent {
 
+  countryService = inject(CountryService);
 
+  onSearch(query:string){
+    console.log({query});
+    this.countryService.searchByCapital(query)
+    .subscribe((resp)=>{
+      console.log({resp});
+    });
+  }
 
  }
