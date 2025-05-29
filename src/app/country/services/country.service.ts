46d89cdc0656c9b70d4b9ba577cbcb762afcc290
soycarlosherrera <carlos4505@gmail.com>
2025-05-28 20:40:01 -0500

    Add catch error in search country

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -19,7 +19,13 @@ export class CountryService {
       .pipe(
         map(restCountries =>
           CountryMapper.restCountryArrayToCountryArray(restCountries)
-        )
+        ),
+        catchError((error) => {
+          console.log('Error fetching ',error);
+          return throwError(
+            ()=> new Error(`No se encontraron coincidencias con: ${query}`)
+          );
+        })
       );
   }
 
